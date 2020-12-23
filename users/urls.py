0c8476a6303b1f138ac5143074d5244eb0c26b6c
Wytze Gelderloos <wytzepakito@gmail.com>
2020-12-24 00:46:30 +0100

    Added JWT authentication and working login

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CustomUserRegistration
-
+from .views import CustomUserRegistration, BlacklistTokenUpdateView
 
 app_name = 'users'
 
 urlpatterns = [
-    path('register/', CustomUserRegistration.as_view(), name='create_user')
+    path('register/', CustomUserRegistration.as_view(), name='create_user'),
+    path('logout/blacklist/', BlacklistTokenUpdateView.as_view(),
+         name='blacklist')
 ]
