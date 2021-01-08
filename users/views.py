2b17f901d52c86f95a591351d6eb2a8a73ad6108
Wytze Gelderloos <wytzepakito@gmail.com>
2021-01-08 19:41:43 +0100

    Added JWT authentication

---
+++ users/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from users.serializers import RegisterUserSerializer
+
+
+class CustomUserRegistration(APIView):
+    permission_classes = [AllowAny]
+
+
+    def post(self, request):
+        reg_serializer = RegisterUserSerializer(data=request.data)
+        if reg_serializer.is_valid():
+            new_user = reg_serializer.save()
+            if new_user:
+                return Response(status=status.HTTP_201_CREATED)
+        return Response(reg_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class BlacklistTokenUpdateView(APIView):
+    permission_classes = [AllowAny]
+    authentication_classes = ()
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
